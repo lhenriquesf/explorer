a9e9c86a0916ba3ebe892caec1e25b0939dcb069
luiz henrique <franca.luiz1@hotmail.com>
2022-12-12 21:09:50 -0300

    Details Finished

---
+++ Stages/Stage 8/frontend/rocketnotes/src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Profile, Logout } from "./styles";
+
+export function Header(){
+    return(
+        <Container>
+            <Profile>
+                <img src="https://github.com/lhenriquesf.png" alt="Foto do usuario"/>
+
+                <div>
+                    <span>Bem vindo</span>
+                    <strong>Luiz Henrique</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
+        </Container>
+    );
+}
